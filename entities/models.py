72ca91b96523d7fb92e13ea999611783d4692429
gutentag2012 <joshua@gawenda.de>
2021-05-23 22:08:50 +0200

    Added Poll model

--- entities/models.py
+++ entities/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
 
 # Create your models here.
+class Poll(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.TextField(max_length=240)
+    start_date = models.DateField(default=datetime.now)
+    end_date = models.DateField(null=True, blank=True)
